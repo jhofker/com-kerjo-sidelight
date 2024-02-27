f06d2fad8cdc63149d9e9da1ad1882d54c6fabed
Jordan Hofker <jhofker@gmail.com>
2024-02-27 09:17:55 -0600

    Remove old package

--- src/styles/styles.11ty.js
+++ src/styles/styles.11ty.js
@@ -22,7 +22,6 @@ module.exports = class {
   async render({ rawCss, rawFilepath }) {
     return await postcss([
       // require('postcss-comment'),
-      require('precss'),
       require('postcss-import'),
       require('postcss-mixins'),
       require('postcss-color-mix'),
